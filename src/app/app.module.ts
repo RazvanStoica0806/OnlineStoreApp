06bc367901f6b0cbcdaf66bf3055cdfca84d1afb
Razvan Stoica <razvanstoica0806@gmail.com>
2019-06-16 00:01:50 +0300

    advancedCss_angularDoc  - update the project to angular 8  - began to do the trilo app from advances css and sass course and at the same time keep the functionality from the angular official doc store app  - stopped because the svg's are not loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
