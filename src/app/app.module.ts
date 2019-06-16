e6fa4e2744a4f2c6a0288e11bce73970d3829ee0
Razvan Stoica <razvanstoica0806@gmail.com>
2019-06-16 13:11:54 +0300

    advancedCss_angularDoc - svg works with background-image css property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {AngularSvgIconModule} from 'angular-svg-icon';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
 import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
+
 
 
 @NgModule({
@@ -24,6 +26,7 @@ import {TopBarComponent} from './top-bar/top-bar.component';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
+    [ HttpClientModule, AngularSvgIconModule ],
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId', component: ProductDetailsComponent},
