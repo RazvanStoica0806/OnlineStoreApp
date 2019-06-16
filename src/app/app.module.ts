318debe26fa10f141de8642fd958f919851f150c
Razvan Stoica <razvanstoica0806@gmail.com>
2019-06-16 13:15:03 +0300

    - uninstall svg-module + delete it from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import {NgModule} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {AngularSvgIconModule} from 'angular-svg-icon';
-import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
 import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
-
 
 
 @NgModule({
@@ -26,7 +22,6 @@ import {TopBarComponent} from './top-bar/top-bar.component';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    [ HttpClientModule, AngularSvgIconModule ],
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId', component: ProductDetailsComponent},
